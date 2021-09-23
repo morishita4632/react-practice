89bad679dd886e8f9acb28f83933d31a6e269bfd
morishita4632 <t.m.thp.hnk@gmail.com>
2021-09-23 23:06:08 +0900

    .

--- textbook_js/module1.js
+++
@@ -1,9 +0,0 @@
-// 名前付きエクスポート
-let var1 = 1;
-let func1 = () => 1;
-
-export { var1, func1 };
-
-// 宣言と同時にエクスポートも可能
-export let var2 = 2;
-export let func2 = () => 2;
--- textbook_js/module2.js
+++
@@ -1,6 +0,0 @@
-// デフォルトエクスポート。ファイルごとに1つだけなので、宣言と同時にexportする場合は関数名（func3）は省略可。変数の場合は同時はできない（∵複数同時に作成できるため）。
-export default function func3() {
-  return 3;
-}
-
-// export default () => 3; // とも書ける
