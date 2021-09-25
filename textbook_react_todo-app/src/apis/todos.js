04f36648827d815495be57efaf355eedea8f5a26
morishita4632 <t.m.thp.hnk@gmail.com>
2021-09-25 21:48:11 +0900

    .

--- textbook_react_todo-app/src/apis/todos.js
+++ textbook_react_todo-app/src/apis/todos.js
@@ -1,1 +1,35 @@
-// サーバーとの通信用ファイル
+/*  サーバーとの通信用ファイル
+    axiosでは以下のメソッドが利用できる
+    ・get(URL, {params: queries}) 取得
+    ・post(URL, newData)          追加
+    ・delete(URL/id)              削除
+    ・put(URL/id, modifyData)     更新
+    各関数のreturnはthenで使える。
+*/
+
+import axios from "axios";
+const todoDataUrl = "http://localhost:3100/todos";
+
+// getで取得
+export const getAllTodosData = async () => {
+  const response = await axios.get(todoDataUrl);
+  return response.data;
+}
+
+// postで追加
+export const addTodoData = async (todo) => {
+  const response = await axios.post(todoDataUrl, todo);
+  return response.data;
+}
+
+// deleteで削除
+export const deleteTodoData = async (id) => {
+  await axios.delete(`${todoDataUrl}/${id}`);
+  return id;
+}
+
+// putで更新
+export const updateTodoData = async (id, todo) => {
+  const response = await axios.put(`${todoDataUrl}/${id}`, todo);
+  return response.data;
+}
--- textbook_react_todo-app/src/hooks/useTodo.js
+++ textbook_react_todo-app/src/hooks/useTodo.js
@@ -1,1 +1,61 @@
 // TODOの状態を管理するカスタムフック
+import { useState, useEffect } from "react";
+import { ulid } from "ulid";
+import * as todoData from "../apis/todos";
+
+export const useTodo = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  // サーバからデータ取得
+  useEffect(() => {
+    todoData.getAllTodosData().then(
+      // thenのコールバック関数は、元の関数のreturnを受け取れる
+      (todo) => {
+        // スプレッド構文を利用して更新判定にかける。また新しいTODOを上に持ってくるためreverseする
+        setTodoList([...todo].reverse());
+      }
+    )
+  }, []);
+
+  /*  完了/未完了のトグル
+      更新後のtodoItemを作成
+      -> サーバに反映
+      -> ローカル側のtodoItemも更新
+      の順に処理
+      （先にローカルを更新だとマズイのか？）
+      thenの中ではtodos.jsで指定した返り値を受け取れる
+  */
+  const toggleTodoListItemStatus = (id, done) => {
+    const todoItem = todoList.find((item) => item.id === id);
+    const newTodoItem = { ...todoItem, done: !done };
+
+    todoData.updateTodoData(id, newTodoItem).then((updatedTodo) => {
+      const newTodoList = todoList.map((item) =>
+        item.id !== updatedTodo.id ? item : updatedTodo
+      );
+      setTodoList(newTodoList);
+    })
+  }
+
+  const addTodoListItem = (todoContent) => {
+    const newTodoItem = { content: todoContent, id: ulid(), done: false };
+    // このreturnは何を返している？
+    return todoData.addTodoData(newTodoItem).then((addTodo) => {
+      setTodoList([addTodo, ...todoList]); // 追加したtodoを前に挿入
+    });
+  }
+
+  const deleteTodoListItem = (id) => {
+    todoData.deleteTodoData(id).then((deleteListItemId) => {
+      const newTodoList = todoList.filter((item) =>
+        item.id !== deleteListItemId
+      );
+      setTodoList(newTodoList);
+    })
+  }
+
+  return {
+    todoList, toggleTodoListItemStatus,
+    addTodoListItem, deleteTodoListItem
+  }
+}
