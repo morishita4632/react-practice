5641f90e18ee2f14eb83ff774e52aea1929873d9
morishita4632 <t.m.thp.hnk@gmail.com>
2021-09-26 16:36:23 +0900

    .

--- textbook_react_todo-app/src/components/TodoItem.js
+++ textbook_react_todo-app/src/components/TodoItem.js
@@ -1,4 +1,8 @@
 // TODO単体用のコンポーネント
+import { ListItem, Text, Flex, Button, IconButton }
+  from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+
 export const TodoItem = ({ todo, toggleTodoListItemStatus, deleteTodoListItem }) => {
   // onClickに直接書くのはよくない？
   const handleToggleTodoListItemStatus = () =>
@@ -6,13 +10,26 @@ export const TodoItem = ({ todo, toggleTodoListItemStatus, deleteTodoListItem })
 
   const handleDeleteTodoListItem = () => deleteTodoListItem(todo.id);
 
-  return <li>
-    {todo.content}
-    <button onClick={handleToggleTodoListItemStatus}>
-      {todo.done ? "未完了リストへ" : "完了リストへ"}
-    </button>
-    <button onClick={handleDeleteTodoListItem}>
-      削除
-    </button>
-  </li>
+  const label = todo.done ? "未完了リストへ" : "完了リストへ";
+  const setColorScheme = todo.done ? "pink" : "blue";
+
+  return (
+    <ListItem
+      borderWidth="1px" p="4" mt="4" bg="white"
+      borderRadius="md" borderColor="gray.300"
+    >
+      <Text mb="6">{todo.content}</Text>
+      <Flex align="center" justify="flex-end">
+        <Button onClick={handleToggleTodoListItemStatus}
+          colorScheme={setColorScheme}
+          variant="outline" size="sm"
+        >
+          {label}
+        </Button>
+        <IconButton onClick={handleDeleteTodoListItem}
+          icon={<DeleteIcon />} variant="unstyled"
+          aria-label="delete"
+        />
+      </Flex>
+    </ListItem>);
 }
--- textbook_react_todo-app/src/components/TodoList.js
+++ textbook_react_todo-app/src/components/TodoList.js
@@ -2,12 +2,17 @@
 import { TodoTitle } from "./TodoTitle";
 import { TodoItem } from "./TodoItem";
 
+import { List } from "@chakra-ui/react";
+
 export const TodoList = ({
   todoList, toggleTodoListItemStatus,
-  deleteTodoListItem, title, as }) => <>
+  deleteTodoListItem, title, as,
+  fontSize }) => <>
     {todoList.length !== 0 && <> {/* 空でないときのみ表示 */}
-      <TodoTitle title={title} as={as} />
-      <ul>
+      <TodoTitle title={title} as={as}
+        fontSize={fontSize} mt="12"
+      />
+      <List w="full">
         {todoList.map((todo) => (
           <TodoItem todo={todo}
             key={todo.id}
@@ -15,7 +20,7 @@ export const TodoList = ({
             deleteTodoListItem={deleteTodoListItem}
           />
         ))}
-      </ul>
+      </List>
     </>
     }
   </>
